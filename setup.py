4839cef0ea68fc0458c703ddf1311faf38b2bfe0
glenfant <gilles.lenfant@gmail.com>
2010-08-20 14:02:25 +0000

    New release tagged

--- setup.py
+++ setup.py
@@ -14,30 +14,31 @@ long_description = (
     + _textFromPath("Products", "SimpleAlias", "CHANGES")
     + "\n")
 
-setup(name='Products.SimpleAlias',
-      version=version,
-      description="SimpleAlias is a product that let's you create aliases or shortcuts to content somewhere in your portal.",
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          ],
-      keywords='web zope plone alias',
-      author='Danny Bloemendaal',
-      author_email='danny.bloemendaal@informaat.nl',
-      url='http://plone.org/products/simplealias',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+    name='Products.SimpleAlias',
+    version=version,
+    description="SimpleAlias is a product that let's you create aliases or shortcuts to content somewhere in your Plone.",
+    long_description=long_description,
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='web zope plone alias',
+    author='Danny Bloemendaal',
+    author_email='danny.bloemendaal@informaat.nl',
+    url='http://plone.org/products/simplealias',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """
+    )
