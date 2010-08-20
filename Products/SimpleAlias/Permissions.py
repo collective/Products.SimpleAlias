e66689032fbcc54c88fbd9f79ca6100421e0569d
glenfant <gilles.lenfant@gmail.com>
2010-08-20 12:47:50 +0000

    Will be replaced later by original branch

--- Products/SimpleAlias/Permissions.py
+++
@@ -1,9 +0,0 @@
-from AccessControl import ModuleSecurityInfo
-security = ModuleSecurityInfo("Products.SimpleAlias.Permissions")
-
-from Products.CMFCore.permissions import setDefaultRoles
-
-security.declarePublic("ADD_ALIAS_PERMISSION")
-ADD_ALIAS_PERMISSION='SimpleAlias: Add Alias'
-setDefaultRoles(ADD_ALIAS_PERMISSION, ('Contributor', 'Manager', 'Owner'))
-
--- Products/SimpleAlias/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""\
-skeleton tests package
-
-To run all tests:
-$ cd $INSTANCE_HOME
-$ bin/zopectl test --nowarning -s Products.SimpleAlias
-"""
